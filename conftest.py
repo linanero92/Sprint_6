8d0d73fa8872fa4b533d17bfc71de44a9c386096
Alina Kuptsova <linakuptsova92@gmail.com>
2024-11-23 13:30:30 +0100

    broken formatting is fixed

--- conftest.py
+++ conftest.py
@@ -1,1 +1,13 @@
-import pytestfrom selenium import webdriverfrom data import *from selenium.webdriver.firefox.options import Options@pytest.fixture()def driver():    options = Options()    driver = webdriver.Chrome(options=options)    driver.get(URL.main_page_url)    yield driver    driver.quit()
+import pytest
+from selenium import webdriver
+from data import *
+from selenium.webdriver.firefox.options import Options
+
+
+@pytest.fixture()
+def driver():
+    options = Options()
+    driver = webdriver.Chrome(options=options)
+    driver.get(URL.main_page_url)
+    yield driver
+    driver.quit()
--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -1,1 +1,33 @@
-from selenium.webdriver.common.by import Byclass OrderPageLocators:    FOR_WHOM = [By.XPATH, '//div[contains(@class, "Order_Header") and contains(text(), "Для кого самокат")]']    CUSTOMER_FIRST_NAME = [By.XPATH, '//input[contains(@placeholder, "* Имя")]']    CUSTOMER_LAST_NAME = [By.XPATH, '//input[contains(@placeholder, "* Фамилия")]']    CUSTOMER_ADDRESS = [By.XPATH, '//input[contains(@placeholder, "* Адрес: куда привезти заказ")]']    METRO_STATION_FIELD = (By.XPATH, '//input[@class="select-search__input"]')    DROP_DOWN_STATION_NAME = (By.XPATH, '//li[contains(@class, "select-search__row")]//button[.//div[contains(@class, "Order_Text") and text()="Черкизовская"]]')    CUSTOMER_PHONE = [By.XPATH, '//input[contains(@placeholder, "* Телефон: на него позвонит курьер")]']    NEXT_BUTTON = [By.XPATH, '//div[contains(@class, "Order_NextButton")]//button[contains(@class, "Button_Button") and contains(@class, "Button_Middle") and text()="Далее"]']    ABOUT_RENT = [By.XPATH, '//div[contains(@class, "Order_Header") and text()="Про аренду"]']    DELIVERY_DATE_FIELD = [By.XPATH, '//div[contains(@class, "react-datepicker__input-container")]//input[contains(@class, "Input_Input")]']    RENTAL_PERIOD = (By.XPATH, '//div[@class="Dropdown-arrow-wrapper"]/span[@class="Dropdown-arrow"]')    RENTAL_PERIOD_SELECT = (By.XPATH, '//div[@class="Dropdown-option" and text()="трое суток"]')    SCOOTER_COLOR = [By.XPATH, '//div[contains(@class, "Order_Checkboxes")]//label[contains(@class, "Checkbox_Label")]/input[@type="checkbox"]']    COMMENT = [By.XPATH, '//input[contains(@placeholder, "Комментарий для курьера")]']    FORM_ORDER_BUTTON = [By.XPATH, '//div[contains(@class, "Order_Buttons")]//button[contains(@class, "Button_Button") and not(contains(@class, "Button_Inverted")) and text()="Заказать"]']    ORDER_CONFIRM_BUTTON = [By.XPATH, '//button[contains(@class, "Button_Button") and contains(@class, "Button_Middle") and text()="Да"]']    ORDER_PLACED = [By.XPATH, '//div[contains(@class, "Order_ModalHeader") and starts-with(text(), "Заказ оформлен")]']    ORDER_STATUS_BUTTON = [By.XPATH, '//button[contains(@class, "Button_Button") and contains (@class, "Button_Middle") and text()="Посмотреть статус"]']    ORDER_CANCEL_BUTTON = [By.XPATH, '//button[contains(@class, "Button_Button") and contains(@class, "Button_Middle") and text()="Отменить заказ"]']
+from selenium.webdriver.common.by import By
+
+
+class OrderPageLocators:
+
+    FOR_WHOM = [By.XPATH, '//div[contains(@class, "Order_Header") and contains(text(), "Для кого самокат")]']
+    CUSTOMER_FIRST_NAME = [By.XPATH, '//input[contains(@placeholder, "* Имя")]']
+    CUSTOMER_LAST_NAME = [By.XPATH, '//input[contains(@placeholder, "* Фамилия")]']
+    CUSTOMER_ADDRESS = [By.XPATH, '//input[contains(@placeholder, "* Адрес: куда привезти заказ")]']
+    METRO_STATION_FIELD = (By.XPATH, '//input[@class="select-search__input"]')
+    DROP_DOWN_STATION_NAME = (By.XPATH, '//li[contains(@class, "select-search__row")]//button[.//div[contains(@class, '
+                                        '"Order_Text") and text()="Черкизовская"]]')
+    CUSTOMER_PHONE = [By.XPATH, '//input[contains(@placeholder, "* Телефон: на него позвонит курьер")]']
+    NEXT_BUTTON = [By.XPATH, '//div[contains(@class, "Order_NextButton")]//button[contains(@class, "Button_Button") '
+                             'and contains(@class, "Button_Middle") and text()="Далее"]']
+    ABOUT_RENT = [By.XPATH, '//div[contains(@class, "Order_Header") and text()="Про аренду"]']
+    DELIVERY_DATE_FIELD = [By.XPATH, '//div[contains(@class, "react-datepicker__input-container")]//input[contains('
+                                     '@class, "Input_Input")]']
+    RENTAL_PERIOD = (By.XPATH, '//div[@class="Dropdown-arrow-wrapper"]/span[@class="Dropdown-arrow"]')
+    RENTAL_PERIOD_SELECT = (By.XPATH, '//div[@class="Dropdown-option" and text()="трое суток"]')
+    SCOOTER_COLOR = [By.XPATH, '//div[contains(@class, "Order_Checkboxes")]//label[contains(@class, '
+                               '"Checkbox_Label")]/input[@type="checkbox"]']
+    COMMENT = [By.XPATH, '//input[contains(@placeholder, "Комментарий для курьера")]']
+    FORM_ORDER_BUTTON = [By.XPATH, '//div[contains(@class, "Order_Buttons")]//button[contains(@class, '
+                                   '"Button_Button") and not(contains(@class, "Button_Inverted")) and text('
+                                   ')="Заказать"]']
+    ORDER_CONFIRM_BUTTON = [By.XPATH, '//button[contains(@class, "Button_Button") and contains(@class, '
+                                      '"Button_Middle") and text()="Да"]']
+    ORDER_PLACED = [By.XPATH, '//div[contains(@class, "Order_ModalHeader") and starts-with(text(), "Заказ оформлен")]']
+    ORDER_STATUS_BUTTON = [By.XPATH, '//button[contains(@class, "Button_Button") and contains (@class, '
+                                     '"Button_Middle") and text()="Посмотреть статус"]']
+    ORDER_CANCEL_BUTTON = [By.XPATH, '//button[contains(@class, "Button_Button") and contains(@class, '
+                                     '"Button_Middle") and text()="Отменить заказ"]']
