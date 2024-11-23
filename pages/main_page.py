ec273e3a7909edbfd2b9e09876d8f63cc32d5082
Alina Kuptsova <linakuptsova92@gmail.com>
2024-11-23 14:47:46 +0100

    formatting is fixed in page and tests files

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,35 @@
-import allurefrom pages.base_page import BasePagefrom pages.order_page import OrderPagefrom locators.main_page_locators import MainPageLocatorsclass MainPage(BasePage):    @allure.step('Акцептировать куки')    def cookies_accept(self):        self.click_to_element(MainPageLocators.COOKIES_BUTTON)    @allure.step('Клик на вопрос')    def click_to_questions(self, num):        locator_questions_formatted = self.format_locators(MainPageLocators.QUESTIONS, num)        self.scroll_to_element(MainPageLocators.LAST_QUESTIONS)        return self.click_to_element(locator_questions_formatted)    @allure.step('Получить ответ')    def get_answers(self, num):        locator_answers_formatted = self.format_locators(MainPageLocators.ANSWERS, num)        return self.get_text_from_element(locator_answers_formatted)    @allure.step('Создать заказ')    def create_order(self, button_locator):        self.scroll_to_element(button_locator)        self.click_to_element(button_locator)        order_page = OrderPage(self.driver)        order_page.set_order()    @allure.step('Переход на главную страницу по клику на лого самоката')    def get_main_page_via_scooter_logo(self):        self.click_to_element(MainPageLocators.SCOOTER_LOGO)        self.scroll_to_element(MainPageLocators.QUESTIONS_TEXT)        return self.get_text_from_element(MainPageLocators.QUESTIONS_TEXT)
+import allure
+from pages.base_page import BasePage
+from pages.order_page import OrderPage
+from locators.main_page_locators import MainPageLocators
+
+
+class MainPage(BasePage):
+
+    @allure.step('Акцептировать куки')
+    def cookies_accept(self):
+        self.click_to_element(MainPageLocators.COOKIES_BUTTON)
+
+    @allure.step('Клик на вопрос')
+    def click_to_questions(self, num):
+        locator_questions_formatted = self.format_locators(MainPageLocators.QUESTIONS, num)
+        self.scroll_to_element(MainPageLocators.LAST_QUESTIONS)
+        return self.click_to_element(locator_questions_formatted)
+
+    @allure.step('Получить ответ')
+    def get_answers(self, num):
+        locator_answers_formatted = self.format_locators(MainPageLocators.ANSWERS, num)
+        return self.get_text_from_element(locator_answers_formatted)
+
+    @allure.step('Создать заказ')
+    def create_order(self, button_locator):
+        self.scroll_to_element(button_locator)
+        self.click_to_element(button_locator)
+        order_page = OrderPage(self.driver)
+        order_page.set_order()
+
+    @allure.step('Переход на главную страницу по клику на лого самоката')
+    def get_main_page_via_scooter_logo(self):
+        self.click_to_element(MainPageLocators.SCOOTER_LOGO)
+        self.scroll_to_element(MainPageLocators.QUESTIONS_TEXT)
+        return self.get_text_from_element(MainPageLocators.QUESTIONS_TEXT)
--- pages/order_page.py
+++ pages/order_page.py
@@ -1,1 +1,46 @@
-import allurefrom pages.base_page import BasePagefrom locators.order_page_locators import OrderPageLocatorsfrom helpers import Generatorsclass OrderPage(BasePage):    @allure.step('Ввод данных клиента')    @allure.step('Заполнено поле "Имя"')    @allure.step('Заполнено поле "Фамилия"')    @allure.step('Заполнено поле "Адрес: куда привезти заказ"')    @allure.step('Выбрана станция метро')    @allure.step('Заполнено поле "Телефон"')    @allure.step('Клик по кнопке "Далее"')    @allure.step('Заполнено поле с датой "Куда привезти заказ"')    @allure.step('Выбран период Аренды')    @allure.step('Выбран цвет самоката')    @allure.step('Заполнено поле "Комментарий для курьера"')    @allure.step('Клик по кпопкам "Заказать" и "Да"')    @allure.step('Клик по кнопке "Посмотреть статус"')    @allure.description('Заполнение и отправка формы с данными клиента')    def set_order(self):        generators = Generators()        first_name = generators.generate_first_name()        self.add_text_to_element(OrderPageLocators.CUSTOMER_FIRST_NAME, first_name)        last_name = generators.generate_last_name()        self.add_text_to_element(OrderPageLocators.CUSTOMER_LAST_NAME, last_name)        address = generators.generate_address()        self.add_text_to_element(OrderPageLocators.CUSTOMER_ADDRESS, address)        self.click_to_element(OrderPageLocators.METRO_STATION_FIELD)        self.click_to_element(OrderPageLocators.DROP_DOWN_STATION_NAME)        phone_number = generators.generate_phone_number()        self.add_text_to_element(OrderPageLocators.CUSTOMER_PHONE, phone_number)        self.click_to_element(OrderPageLocators.NEXT_BUTTON)        delivery_data = generators.generate_delivery_date()        self.add_text_to_element(OrderPageLocators.DELIVERY_DATE_FIELD, delivery_data)        self.click_to_element(OrderPageLocators.RENTAL_PERIOD)        self.click_to_element(OrderPageLocators.RENTAL_PERIOD_SELECT)        scooter_color = generators.generate_scooter_color()        self.add_text_to_element(OrderPageLocators.SCOOTER_COLOR, scooter_color)        comment = generators.generate_comment()        self.add_text_to_element(OrderPageLocators.COMMENT, comment)        self.click_to_element(OrderPageLocators.FORM_ORDER_BUTTON)        self.click_to_element(OrderPageLocators.ORDER_CONFIRM_BUTTON)        self.click_to_element(OrderPageLocators.ORDER_STATUS_BUTTON)
+import allure
+from pages.base_page import BasePage
+from locators.order_page_locators import OrderPageLocators
+from helpers import Generators
+
+
+class OrderPage(BasePage):
+
+    @allure.step('Ввод данных клиента')
+    @allure.step('Заполнено поле "Имя"')
+    @allure.step('Заполнено поле "Фамилия"')
+    @allure.step('Заполнено поле "Адрес: куда привезти заказ"')
+    @allure.step('Выбрана станция метро')
+    @allure.step('Заполнено поле "Телефон"')
+    @allure.step('Клик по кнопке "Далее"')
+    @allure.step('Заполнено поле с датой "Куда привезти заказ"')
+    @allure.step('Выбран период Аренды')
+    @allure.step('Выбран цвет самоката')
+    @allure.step('Заполнено поле "Комментарий для курьера"')
+    @allure.step('Клик по кпопкам "Заказать" и "Да"')
+    @allure.step('Клик по кнопке "Посмотреть статус"')
+    @allure.description('Заполнение и отправка формы с данными клиента')
+    def set_order(self):
+        generators = Generators()
+        first_name = generators.generate_first_name()
+        self.add_text_to_element(OrderPageLocators.CUSTOMER_FIRST_NAME, first_name)
+        last_name = generators.generate_last_name()
+        self.add_text_to_element(OrderPageLocators.CUSTOMER_LAST_NAME, last_name)
+        address = generators.generate_address()
+        self.add_text_to_element(OrderPageLocators.CUSTOMER_ADDRESS, address)
+        self.click_to_element(OrderPageLocators.METRO_STATION_FIELD)
+        self.click_to_element(OrderPageLocators.DROP_DOWN_STATION_NAME)
+        phone_number = generators.generate_phone_number()
+        self.add_text_to_element(OrderPageLocators.CUSTOMER_PHONE, phone_number)
+        self.click_to_element(OrderPageLocators.NEXT_BUTTON)
+        delivery_data = generators.generate_delivery_date()
+        self.add_text_to_element(OrderPageLocators.DELIVERY_DATE_FIELD, delivery_data)
+        self.click_to_element(OrderPageLocators.RENTAL_PERIOD)
+        self.click_to_element(OrderPageLocators.RENTAL_PERIOD_SELECT)
+        scooter_color = generators.generate_scooter_color()
+        self.add_text_to_element(OrderPageLocators.SCOOTER_COLOR, scooter_color)
+        comment = generators.generate_comment()
+        self.add_text_to_element(OrderPageLocators.COMMENT, comment)
+        self.click_to_element(OrderPageLocators.FORM_ORDER_BUTTON)
+        self.click_to_element(OrderPageLocators.ORDER_CONFIRM_BUTTON)
+        self.click_to_element(OrderPageLocators.ORDER_STATUS_BUTTON)
